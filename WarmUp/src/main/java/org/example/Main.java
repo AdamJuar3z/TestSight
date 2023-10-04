08d17e18c21688d3d865373956ab39bd447506b8
Adam Juarez <adamjuarez@Adams-MacBook-Pro.local>
2023-10-04 10:33:45 -0500

    WarmUp

---
+++ WarmUp/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+      String personName = "John Doe";
+      int personAge = 25;
+      double productPrice = 19.99;
+      float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = true;
+
+
+    }
+}

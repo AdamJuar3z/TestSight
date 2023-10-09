4e2d3264a7b0b473aa03e94ebe67c9e4e4ea7104
Adam Juarez <adamjuarez@Adams-MacBook-Pro.local>
2023-10-09 00:59:58 -0500

    Calculator 3

--- Week1/src/main/java/Cal/Example3/Example3.java
+++ Week1/src/main/java/Cal/Example3/Example3.java
@@ -22,7 +22,7 @@ public class Example3 {
         rate = scanner.nextDouble();
 
         amount = priciple * Math.pow((1 + time / 100 ), rate * 12);
-        
+
         System.out.printf("To fund an annuity that pays %.2f monthly for %.2f years and earns an expected %.2f interest, you would need to invest %.2f today.", priciple, time, rate, amount);
 
 
